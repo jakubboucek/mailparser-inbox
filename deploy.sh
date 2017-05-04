309a235305e63c1923865977ce784cb17585408e
Jakub Bouček <pan@jakubboucek.cz>
2017-05-04 06:53:52 +0200

    Complete deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REMOTE_DIR="/var/www/cortis.ion.cz/www"
+REMOTE_DIR="/var/www/cortis.ion.cz/"
 LOCAL_DIR=""
 SERVER_NAME="sbl"
 
@@ -13,16 +13,16 @@ rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}${LOCAL_DIR}/"
 echo "Replace file permissions…"
 $SSH sudo fixwww.sh $REMOTE_DIR
 
-# echo "Remove temporary files…"
-# $SSH find $REMOTE_DIR/temp -mindepth 2 -type f -delete
+echo "Remove temporary files…"
+$SSH find $REMOTE_DIR/temp -mindepth 2 -type f -delete
 
-# echo -n "Remove nette email-sent marker… "
-# ssh sbl /bin/bash << EOF
-# 	if [ -f ${REMOTE_DIR}/log/email-sent ]
-# 	then
-# 		rm ${REMOTE_DIR}/log/email-sent
-# 		echo "removed"
-# 	else
-# 		echo "no exists"
-# 	fi
-# EOF
+echo -n "Remove nette email-sent marker… "
+ssh sbl /bin/bash << EOF
+	if [ -f ${REMOTE_DIR}/log/email-sent ]
+	then
+		rm ${REMOTE_DIR}/log/email-sent
+		echo "removed"
+	else
+		echo "no exists"
+	fi
+EOF
